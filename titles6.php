1143125fade733a001bf8604a84e5b14b476c588
SHREYAS1009 <shreyas0910@gmail.com>
2022-04-21 17:46:53 +0530

    added comments in titles6.php

--- titles6.php
+++ titles6.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-  session_start();
-  if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
-    exit;
-  }
-}
+// if (session_status() == PHP_SESSION_NONE) {
+//   session_start();
+//   if (!isset($_SESSION['username'])) {
+//     header("Location: index.php");
+//     exit;
+//   }
+// }
 ?>
 <html>
 
