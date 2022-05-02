6f747e6ccba9083c7bf2595283fba52dfb9d7bd8
Your Name <you@example.com>
2022-05-03 00:01:32 +0530

    added quiz for dbmstheory.php

---
+++ dbmstheory/quiz/dbmstv1data.js
@@ -0,0 +1,31 @@
+var jsonData = [
+    {
+        "q" : "What is the full form of DBMS?",
+        "opt1" : "Database Management System",
+        "opt2" : " Data of Binary Management System",
+        "opt3" : "Database Management Service",
+        "answer" : "Database Management System"
+    },
+    {
+        "q" : "What is a database?",
+        "opt1" : " Organized collection of data or information that can be accessed, updated, and managed",
+        "opt2" : "Collection of data or information without organizing",
+        "opt3" : "Organized collection of data that cannot be updated",
+        "answer" : " Organized collection of data or information that can be accessed, updated, and managed"
+    },
+    {
+        "q" : "Who created the first DBMS?",
+        "opt1" : "Charles Bachman",
+        "opt2" : "Edgar Frank Codd",
+        "opt3" : "Sharon B. Codd",
+        "answer" :"Charles Bachman"
+    },
+    {
+        "q" : "Which of the following is not a type of database?",
+        "opt1" : "Decentralized",
+        "opt2" : "Hierarchical",
+        "opt3" : "Network",
+        "answer" : "Decentralized"
+    }
+   
+];
