cccff54750b5651f197781f3ea7ee531c871808f
Your Name <you@example.com>
2022-05-03 23:31:44 +0530

    aadded quiz to os

--- Automata Theory/quiz/atv5data.js
+++ Automata Theory/quiz/atv5data.js
@@ -1,26 +1,26 @@
 var jsonData = [
     {
-        "q" : "  Which of the following is true?",
-        "opt1" : "(01)*0 = 0(10)*",
-        "opt2" : "(0+1)*0(0+1)*1(0+1) = (0+1)*01(0+1)*",
-        "opt3" : "Both ",
+        "q" : "  ",
+        "opt1" : "",
+        "opt2" : "",
+        "opt3" : " ",
         
         
-        "answer" : "Both"
+        "answer" : ""
     },
     {
-        "q" : "A language is regular if and only if ",
-        "opt1" : " accepted by DFA",
-        "opt2" : " accepted by PDA ",
-        "opt3" : " accepted by LBA",
-        "answer" : "accepted by DFA "
+        "q" : " ",
+        "opt1" : " ",
+        "opt2" : "  ",
+        "opt3" : " ",
+        "answer" : " "
     },
     {
-        "q" : " Regular grammar is",
-        "opt1" : " context free grammar",
-        "opt2" : " english grammar  ",
-        "opt3" : " non context free grammar",
-        "answer" :"  context free grammar"
+        "q" : " ",
+        "opt1" : " ",
+        "opt2" : "   ",
+        "opt3" : " ",
+        "answer" :"  "
     }
 
     
