cf1892439d84f7942cb195d08c6079ec3aff5d44
Your Name <you@example.com>
2022-05-04 21:17:24 +0530

    added quiz to microprocessor.php

---
+++ Microprocessor/quiz/mpv4data.js
@@ -0,0 +1,31 @@
+var jsonData = [
+    {
+        "q" : "If an interrupt is generated from outside the processor then it is an",
+        "opt1" : "external interrupt",
+        "opt2" : " internal interrupt",
+        "opt3" : " interrupt",
+        "answer" : "external interrupt"
+    },
+    {
+        "q" : "Example of an external interrupt is",
+        "opt1" : " keyboard interrupt",
+        "opt2" : " divide by zero interrupt",
+        "opt3" : "type2 interrupt",
+        "answer" : " keyboard interrupt"
+    },
+    {
+        "q" : "The interrupt request that is independent of IF flag is",
+        "opt1" : "TRAP",
+        "opt2" : "NMI",
+        "opt3" : "Both ",
+        "answer" :"Both"
+    },
+    {
+        "q" : "When the CPU executes IRET,",
+        "opt1" : "contents of IP and CS are retrieved",
+        "opt2" : "clears the interrupt flag",
+        "opt3" : "the control transfers from ISR to main program",
+        "answer" : "contents of IP and CS are retrieved"
+    }
+   
+];
---
+++ Microprocessor/quiz/mpv5data.js
@@ -0,0 +1,31 @@
+var jsonData = [
+    {
+        "q" : "Programmable peripheral input-output port is another name for",
+        "opt1" : "parallel input-output port",
+        "opt2" : "serial input-output port ",
+        "opt3" : "parallel output port ",
+        "answer" : "parallel input-output port"
+    },
+    {
+        "q" : " The data bus buffer is controlled by",
+        "opt1" : " read/write control logic",
+        "opt2" : "control word register",
+        "opt3" : "data bus",
+        "answer" : " read/write control logic"
+    },
+    {
+        "q" : "The port that is used for the generation of handshake lines in mode 1 or mode 2 is",
+        "opt1" : "port C Upper",
+        "opt2" : "port A",
+        "opt3" : "port B",
+        "answer" :"port C Upper"
+    },
+    {
+        "q" : "If A1=0, A0=1 then the input read cycle is performed from",
+        "opt1" : " port B to data bus",
+        "opt2" : "CWR to data bus",
+        "opt3" : "None",
+        "answer" : " port B to data bus"
+    }
+   
+];
