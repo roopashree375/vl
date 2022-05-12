55a479834a129cdb1e64b3e9bb46fe23f0c1288b
roopashree375 <62794413+roopashree375@users.noreply.github.com>
2022-05-12 22:39:25 +0530

    login backend

--- process/loginprocess.php
+++ process/loginprocess.php
@@ -1,11 +1,22 @@
 <?php
-$servername = "localhost:3307";
-$db_username = "root";
-$password = "";
-$dbname = "demoproject";
+// $servername = "localhost:3307";
+// $db_username = "root";
+// $password = "";
+// $dbname = "demoproject";
 
-// Create connection
-$conn = new mysqli($servername, $db_username, $password, $dbname);
+// // Create connection
+// $conn = new mysqli($servername, $db_username, $password, $dbname);
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url("mysql://be39a3f3c6dc73:36a3f8be@us-cdbr-east-05.cleardb.net/heroku_d9e1dea0ab8f11f?reconnect=true");
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 // Check connection
 if ($conn->connect_error) {
