726b0636f1b5d7f0e3116b013eb2cdaf2e591ab8
RithvikBhat <code.rithvikkbhat@gmail.com>
2022-06-17 20:26:22 +0530

    Edited 3rd sem

--- OppsWithJava/generic.php
+++ OppsWithJava/generic.php
@@ -1,16 +1,15 @@
 <?php
- if(session_status()==PHP_SESSION_NONE)
- {
-   session_start();
-   if(!isset($_SESSION['username']))
-   {
-     header("Location: ../index.php");
-     exit;
-   }
- }
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+  if (!isset($_SESSION['username'])) {
+    header("Location: ../index.php");
+    exit;
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,88 +22,91 @@
   <link rel="stylesheet" href="../css/home.css">
   <link rel="icon" href="../images/dscelogo.png">
   <title>Generic Class</title>
-  </head>
+</head>
+
 <body>
-<h1 class="ui header" style="text-align:center" id = "head">
-      <a class="ui circular image" href="http://dsce.edu.in"><img src="../images/dscelogo.jpg"></a>
-      <a href="titles.php">Virtual Labs</a> 
-  <br><p style="font-size:25px;margin-top:10px; text-align: center;">A Self Learning initiative by the Department of Computer Science</p>
-</h1>
+  <h1 class="ui header" style="text-align:center" id="head">
+    <a class="ui circular image" href="http://dsce.edu.in"><img src="../images/dscelogo.jpg"></a>
+    <a href="titles.php">Virtual Labs</a>
+    <br>
+    <p style="font-size:25px;margin-top:10px; text-align: center;">A Self Learning initiative by the Department of Computer Science</p>
+  </h1>
   <div id="toTop"><i class="chevron up icon"></i></div>
   <div>
-  <div class="ui  light grey inverted secondary huge menu">
-  <a class="header item" href="../index.php">
-    Home
-  </a>
-  <a class="item" href="OopsWithJava.php">
-  Oops With Java
-  </a>
- <a class="item">
-    About Us
-  </a> 
- <?php
-    // if(!isset($_SESSION["username"])) echo "<a class=\"item\" href=\"../login.php\">Login</a><a class=\"item\" href=\"../register.php\">Register</a>";
-    // else 
-    // { 
-    //   echo "<a class=\"item\">"; 
-    //   echo $_SESSION['username'];
-    //   echo "</a><a class=\"item\" href=\"../logoutprocess.php\">Logout</a>";
-    // }
-    ?>
-</div>
-</div>
-  <div class="ui container" id="cont">
-    <h2 class="ui header" style="font-size:35px; margin-left:10px;">
-    Generic Class
-    </h2>
-    <div class="ui stackable grid">
-  <div class="four wide column">
-    <div class="ui secondary vertical pointing menu" id="Menus">
-      <a class="active item" id="intro">
-        Introduction
+    <div class="ui  light grey inverted secondary huge menu">
+      <a class="header item" href="../index.php">
+        Home
       </a>
-      <a class="item" id="prereq">
-        Theory
+      <a class="item" href="OopsWithJava.php">
+        Oops With Java
       </a>
-      <!-- <a class="item" target="_blank" id="list">
-        Compiler
-      </a> -->
-      <a class="item" id="faq">
-        Quiz
+      <a class="item">
+        About Us
       </a>
+      <?php
+      // if(!isset($_SESSION["username"])) echo "<a class=\"item\" href=\"../login.php\">Login</a><a class=\"item\" href=\"../register.php\">Register</a>";
+      // else 
+      // { 
+      //   echo "<a class=\"item\">"; 
+      //   echo $_SESSION['username'];
+      //   echo "</a><a class=\"item\" href=\"../logoutprocess.php\">Logout</a>";
+      // }
+      ?>
     </div>
   </div>
- <div class="twelve wide stretched column">
-    <div class="ui segment">
-      
-      <div id="int">
-      Write a java program to implement a stack using generic class and methods.
-        <iframe style="width:100%; height:100%;" allowfullscreen="allowfullscreen" src="https://www.youtube.com/embed/XMvznsY02Mk">
-</iframe><br><br>
-In this program, you'll learn to use gerneric and meathods of stack
+  <div class="ui container" id="cont">
+    <h2 class="ui header" style="font-size:35px; margin-left:10px;">
+      Generic Class
+    </h2>
+    <div class="ui stackable grid">
+      <div class="four wide column">
+        <div class="ui secondary vertical pointing menu" id="Menus">
+          <a class="active item" id="intro">
+            Introduction
+          </a>
+          <a class="item" id="prereq">
+            Theory
+          </a>
+          <!-- <a class="item" target="_blank" id="list">
+        Compiler
+      </a> -->
+          <a class="item" id="faq">
+            Quiz
+          </a>
+        </div>
       </div>
-      
-      
-      <div id="pre" style="display: none;">
-        <div class="ui bulleted list">
-          <div class="item">
-          
-          <div class="GrayBlock">
-          <br>
-            <img id="theoryImage" src="../images/string.png">
-          <br>
-          </div></div>
-            
-          </div>
-          <br>
-      </div>
+      <div class="twelve wide stretched column">
+        <div class="ui segment">
 
-      
-      <div id="lis" style="display: none; ">
-      <div class="ui form">
-        <div id="compilerData">
-         <b>Compile your java program here<b>
-         <!--</div><br>
+          <div id="int">
+            Write a java program to implement a stack using generic class and methods.
+            <iframe height="400px" width="100%" allowfullscreen="allowfullscreen" src="https://www.youtube.com/embed/XMvznsY02Mk">
+            </iframe><br><br>
+            In this program, you'll learn to use gerneric and meathods of stack
+          </div>
+
+
+          <div id="pre" style="display: none;">
+            <div class="ui bulleted list">
+              <div class="item">
+
+                <div class="GrayBlock">
+                  <br>
+                  <img id="theoryImage" src="../images/string.png">
+                  <br>
+                </div>
+              </div>
+
+            </div>
+            <br>
+          </div>
+
+
+          <div id="lis" style="display: none; ">
+            <div class="ui form">
+              <div id="compilerData">
+                <b>Compile your java program here<b>
+                    <!--</div><br>
           <form action="../compile.php" id="form" name="f2" method="POST" ><br>
           <label class="writeCode">Write Your Code</label><br><br>     
           <textarea name="code" rows=15 cols=80 onkeydown=insertTab(this,event) class="field codeBlock" id="editor code"rows="10" cols="50">
@@ -133,32 +135,32 @@ class Simple
           </form><br>
           <div id="outputBox" style="display:None;">Output:<br><br>
           <div name="output"></div><br>-->
-          </div>
+              </div>
+            </div>
+
           </div>
 
-      </div>
-      
-      <div id="faqs" style="display: none;">
-      <a href="quiz/genericquiz.php"><button id="quizbutton"><strong>Take a Quiz and Test your knowledge</strong></button></a>
+          <div id="faqs" style="display: none;">
+            <a href="quiz/genericquiz.php"><button id="quizbutton"><strong>Take a Quiz and Test your knowledge</strong></button></a>
+          </div>
         </div>
       </div>
-      </div>
-      
+
     </div>
   </div>
-</div>
+  </div>
   </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/ace.js" type="text/javascript" charset="utf-8"></script>
-<script>
+  <script>
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode("ace/mode/c_cpp");
     var textarea = $('textarea[name="editor"]');
-editor.getSession().on("change", function () {
-    textarea.val(editor.getSession().getValue());
-});
-</script>
-  
+    editor.getSession().on("change", function() {
+      textarea.val(editor.getSession().getValue());
+    });
+  </script>
+
 </body>
+
 </html>
-      
