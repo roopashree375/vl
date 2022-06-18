2e73dca691b54ac17d24991a6245d41047b3a4a7
RithvikBhat <code.rithvikkbhat@gmail.com>
2022-06-18 09:01:24 +0530

    Commit

--- CC/ccquiz/quiz5.js
+++ CC/ccquiz/quiz5.js
@@ -1,45 +1,45 @@
-var jsonData=[
+var jsonData = [
     {
-        "q":"Which provides least amount of security?",
-        "opt1":"SAAS",
-        "opt2":"PAAS",
-        "opt3":"IAAS",
-        "answer":"IAAS",
+        "q": "Which provides least amount of security?",
+        "opt1": "SAAS",
+        "opt2": "PAAS",
+        "opt3": "IAAS",
+        "answer": "IAAS",
 
 
     },
     {
-        "q":"1. Which of the following is not a  distributed file system?",
-        "opt1":"Unix",
-        "opt2":"Apollo",
-        "opt3":"network file system",
-        "answer":"Unix",
+        "q": "Which of the following is not a  distributed file system?",
+        "opt1": "Unix",
+        "opt2": "Apollo",
+        "opt3": "network file system",
+        "answer": "Unix",
 
     },
     {
 
-        "q":"Which of the following is not a characteristic of a UFS?",
-        "opt1":"Flexibility",
-        "opt2":"Scalability",
-        "opt3":"Elasticity",
-        "answer":"Elasticity",
+        "q": "Which of the following is not a characteristic of a UFS?",
+        "opt1": "Flexibility",
+        "opt2": "Scalability",
+        "opt3": "Elasticity",
+        "answer": "Elasticity",
     },
-    
-    {
-        "q":"Pick the operational domain of CSA?",
-        "opt1":"Scalaility",
-        "opt2":"Portability and interoperability",
-        "opt3":"Flexibility",
-        "answer":"Portability and interoperability",
 
-     },
+    {
+        "q": "Pick the operational domain of CSA?",
+        "opt1": "Scalaility",
+        "opt2": "Portability and interoperability",
+        "opt3": "Flexibility",
+        "answer": "Portability and interoperability",
+
+    },
     {
 
-        "q":"Xoar is based on?",
-        "opt1":"Micro kernel",
-        "opt2":"Micro Processor",
-        "opt3":"None of the above",
-        "answer":"Micro kernel",
+        "q": "Xoar is based on?",
+        "opt1": "Micro kernel",
+        "opt2": "Micro Processor",
+        "opt3": "None of the above",
+        "answer": "Micro kernel",
 
     },
 
